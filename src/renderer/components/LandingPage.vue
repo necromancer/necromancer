1136794d5dbf5fc31a4c47d38df2459a3393bd55
joancipria <joancipria@gmail.com>
2017-12-01 01:37:42 +0100

    Moving to Vue.js. Essential base created.

---
+++ src/renderer/components/LandingPage.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="wrapper">
+      <link rel="preload" href="~@/assets/img/table.jpg" as="image">
+    <div class="title">
+      <img id="logo" src="~@/assets/img/title.png" alt="electron-vue">
+    </div>
+    <main>
+      <div class="menu">
+        <router-link to="singleplayer">
+          <img class="singleplayer-button" src="~@/assets/img/buttons/NSinglePlayer.png" alt="">
+        </router-link>
+        <img onclick="#" src="~@/assets/img/buttons/BMultiPlayer.png" alt="">
+        <img onclick="#" src="~@/assets/img/buttons/BMyDeck.png" alt="">
+        <img class="options-button" onclick="openOptions();" src="~@/assets/img/buttons/NOptions.png" alt="">
+        <img class="exit-button" onclick="exitApp();" src="~@/assets/img/buttons/NExit.png" alt="">
+      </div>
+
+      <div class="window" id="options" style="display:none;">
+        <div style="text-align:right;color:#901805;" id="newMets_handle">
+          <a onclick="closeWindow(this);">&#x2716;&nbsp;</a>
+        </div>
+        <img class="faces" id="PlayerImageProfile" src="" alt="">
+        <input onkeyup="controlSaveButton(true);" id="playerName" type="text" name="">
+        <br>
+        <select onchange="controlSaveButton(true);" id="deckSelection"></select>
+        <img class="small-back-button back-button" onclick="previousImageProfile();controlSaveButton(true);" src="img/buttons/NBack.png"
+          alt="">
+        <img class="small-back-button next-button" onclick="nextImageProfile();controlSaveButton(true);" src="img/buttons/NNext.png"
+          alt="">
+        <img id="save-button" class="save-button" src="img/buttons/BSave.png" alt="">
+        <br>
+      </div>
+      <audio autoplay="" controls="" loop="" preload="">
+        <source src="~@/assets/audio/music1.ogg" type="audio/ogg"></source>
+        Your browser does not support the audio element.
+      </audio>
+    </main>
+  </div>
+</template>
+
+<script>
+  import SystemInformation from "./LandingPage/SystemInformation";
+
+  export default {
+    name: "landing-page",
+    components: {
+      SystemInformation
+    },
+    methods: {
+      open(link) {
+        this.$electron.shell.openExternal(link);
+      }
+    }
+  };
+</script>
+
+<style>
+  @import "~@/assets/css/landingPage.css";
+</style>
---
+++ src/renderer/components/SinglePlayer.vue
@@ -0,0 +1,82 @@
+<template>
+    <div id="wrapper">
+        <div class="enemy-bar">
+            <img class="enemy-bar-profile" src="~@/assets/img/faces/enemy.png" alt="">
+            <h4>Enemy </h4>
+            <h5 id="enemy-life">60</h5>
+        </div>
+        <div id="battlefield" class="battlefield">
+            <div id="space1" class="card-space">
+                <img src="~@/assets/img/blank.png" alt="">
+            </div>
+            <div id="space2" class="card-space">
+                <img src="~@/assets/img/blank.png" alt="">
+            </div>
+            <div id="space3" class="card-space">
+                <img src="~@/assets/img/blank.png" alt="">
+            </div>
+            <div id="space4" class="card-space">
+                <img src="~@/assets/img/blank.png" alt="">
+            </div>
+            <div id="space5" class="card-space">
+                <img src="~@/assets/img/blank.png" alt="">
+            </div>
+            <div id="space6" class="card-space">
+                <img src="~@/assets/img/blank.png" alt="">
+            </div>
+
+            <br>
+            <br>
+
+            <div id="space7" onclick="moveCard(this);" class="card-space">
+                <img src="~@/assets/img/blank.png" alt="">
+            </div>
+            <div id="space8" onclick="moveCard(this);" class="card-space">
+                <img src="~@/assets/img/blank.png" alt="">
+            </div>
+            <div id="space9" onclick="moveCard(this);" class="card-space">
+                <img src="~@/assets/img/blank.png" alt="">
+            </div>
+            <div id="space10" onclick="moveCard(this);" class="card-space">
+                <img src="~@/assets/img/blank.png" alt="">
+            </div>
+            <div id="space11" onclick="moveCard(this);" class="card-space">
+                <img src="~@/assets/img/blank.png" alt="">
+            </div>
+            <div id="space12" onclick="moveCard(this);" class="card-space">
+                <img src="~@/assets/img/blank.png" alt="">
+            </div>
+        </div>
+        <div class="player-bar w3-light-grey">
+            <div id="player-life" class="w3-green" style="width:100%"></div>
+            <img id="PlayerImageProfile" class="player-bar-profile" src="" alt="">
+
+            <h4 id="playerName"></h4>
+            <!--<h5 id="player-life">60</h5>
+                <div class="allManas">
+                <div class="mana"><img src="~@/assets/img/combat/fire_rune.png" alt=""><span id="fireMana"></span></div>
+                <div class="mana"><img src="~@/assets/img/combat/water_rune.png" alt=""><span id="waterMana"></span></div>
+                <div class="mana"><img src="~@/assets/img/combat/air_rune.png" alt=""><span id="airMana"></span></div>
+                <div class="mana"><img src="~@/assets/img/combat/earth_rune.png" alt=""><span id="earthMana"></span></div>
+                <img onclick="skipTurn();" src="~@/assets/img/buttons/skipTurn.png" alt="" style="
+                top: 7px;
+                width: 2.5%;
+                position: absolute;
+                right: -50px;
+        ">        </div>-->
+        </div>
+        <div id="cards-deck" class="cards-deck"></div>
+        <main>
+            <router-link to="/">
+                <h2>Main Menu!</h2>
+            </router-link>
+
+        </main>
+    </div>
+</template>
+
+
+
+<style>
+    @import "~@/assets/css/singlePlayer.css";
+</style>
