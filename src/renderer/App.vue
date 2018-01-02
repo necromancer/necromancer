f1dd921ee64b5a71576b1ae753b4bbc5674a3e81
Joan Ciprià <joancipria@gmail.com>
2018-01-02 20:31:26 +0100

    Moved global styles here. (Using components style)

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,4 +1,3 @@
-
 <template>
   <div id="app">
     <transition name="fade" mode="out-in" appear>
@@ -6,18 +5,52 @@
     </transition>
   </div>
 </template>
-
 <script>
-  export default {
-    name: "necromancer"
-  };
+export default {
+  name: "Necromancer"
+};
 </script>
 
+<!-- Global Styles -->
 <style>
-.fade-enter-active, .fade-leave-active {
-  transition: opacity 0.3s
+html {
+  cursor: url(~@/assets/img/cursors/default.png), default;
+  -webkit-user-select: none;
 }
 
-.fade-enter, .fade-leave-active {
-  opacity: 0
-}</style>
+:link {
+  cursor: url(~@/assets/img/cursors/pointer.png), pointer;
+}
+body {
+    background-size: cover;
+    padding: 0;
+    margin: 0;
+    overflow:hidden;
+}
+
+a:hover, a:active, a:focus {
+    outline: 0;
+}
+
+.window {
+  position: absolute;
+  width: 40%;
+  height: 40%;
+  top: 30%;
+  left: 30%;
+  text-align: left;
+}
+
+audio {
+  display: none;
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s;
+}
+
+.fade-enter,
+.fade-leave-active {
+  opacity: 0;
+}
+</style>
