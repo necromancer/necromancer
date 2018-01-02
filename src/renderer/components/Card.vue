94b2a06b1c37d5b991f79b22ff98f32caf7bcb96
Joan Ciprià <joancipria@gmail.com>
2018-01-02 20:34:09 +0100

    Creating a Card component (Work in progress)

---
+++ src/renderer/components/Card.vue
@@ -0,0 +1,51 @@
+<template>
+    <div v-bind:id="id" class="card" v-bind:elementaltype="elementaltype" available="true">
+        <img src="decks/default/img/priest.png" alt="card" class="character">
+        <img src="decks/default/img/card.png" alt="card">
+        <div class="life-indicator">{{life}}</div>
+        <div class="attack-indicator">{{attack}}</div>
+        <div class="cost-indicator">{{cost}}</div>
+    </div>
+</template> 
+
+<script>
+export default {
+    name: 'card',
+    props:{
+        id: String,
+        elementaltype: String,
+        life: Number,
+        attack: Number,
+        cost: Number
+    }
+
+};
+</script>
+
+<style scoped>
+.card {
+  box-sizing: border-box;
+  position: relative;
+  display: inline-table;
+  text-align: center;
+  font-size: 70%;
+  font-weight: bold;
+  padding: 0;
+  padding-right: 15%;
+  margin-bottom: 15%;
+}
+
+.card img {
+  padding: 0;
+  margin: 0;
+  position: absolute;
+  /* width: 100%; */
+}
+
+.card .character {
+  margin-top: 8px;
+  margin-left: 3px;
+  max-height: 80px;
+  clip: rect(0px, 80px, 80px, 0px);
+}
+</style>
