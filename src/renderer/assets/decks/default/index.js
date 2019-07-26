dde6a8e5f419c9f71b5953b5a0cea82a0c4f120a
Joan Ciprià <joancipria@gmail.com>
2019-07-26 19:06:58 +0200

    Added shuffle to default deck

--- src/renderer/assets/decks/default/index.js
+++ src/renderer/assets/decks/default/index.js
@@ -2,11 +2,11 @@
 var cards = require(`./cards.json`);
 
 // Elemental card's objects
-var fireCards = filterCards('fire');
-var waterCards = filterCards('water');
-var airCards = filterCards('air');
-var earthCards = filterCards('earth');
-var deathCards = filterCards('death');
+var fireCards = shuffle(filterCards('fire'));
+var waterCards = shuffle(filterCards('water'));
+var airCards = shuffle(filterCards('air'));
+var earthCards = shuffle(filterCards('earth'));
+var deathCards = shuffle(filterCards('death'));
 
 
 // Filter cards by elemental
@@ -15,6 +15,25 @@ function filterCards(elemental) {
         return u.ElementalType == elemental
         })
 }
+
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
+  }
 
 export default {
     fireCards,
